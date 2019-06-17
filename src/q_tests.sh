70f55220597df7b019d64061d8503c725ebf8559
Ramesh Subramonian <subramonian@gmail.com>
2019-06-17 15:48:50 +0000

    ok

--- src/q_tests.sh
+++ src/q_tests.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 set -e 
 rm -f _*
+if [ $# != 0 ] && [ $# != 1 ]; then 
+  echo "ERROR. Usage is $0 or $0 <N> where N is test num"; exit 1; 
+fi
+if [ $# = 1 ]; then
+  TESTNUM=$1
+fi
+
 CFLAGS=" -std=gnu99 -Wall -Wextra -Werror -fopenmp -g -D_GNU_SOURCE -D_DEFAULT_SOURCE -Wno-unknown-warning-option  -Wstrict-prototypes -Wmissing-prototypes -Wpointer-arith -Wmissing-declarations -Wredundant-decls -Wnested-externs -Wshadow -Wcast-qual -Wcast-align -Wwrite-strings -Wold-style-definition -Wsuggest-attribute=noreturn -Wduplicated-cond -Wmisleading-indentation -Wnull-dereference -Wduplicated-branches -Wrestrict "
 VG=" valgrind --leak-check=full --show-leak-kinds=all  "
 
 FILES=" q_rhashmap.c murmurhash.c "
 VG="" # Uncomment this line if you do not want Valgrind to run
 
-ls test_q_rhashmap_*.c > _files
+if [ $TESTNUM = "" ]; then 
+  ls test_q_rhashmap_*.c > _files
+else
+  ls test_q_rhashmap_*.c | grep $TESTNUM > _files
+fi
+nfiles=0
 while read line; do
   echo "Testing $line"
   gcc $CFLAGS $FILES $line -o a.out
@@ -16,17 +28,34 @@ while read line; do
   if [ "$VG" != "" ]; then 
     grep "0 errors from 0 contexts" _err 1>/dev/null 2>&1
   fi
+  nfiles=`expr $nfiles + 1 `=
 done < _files
+if [ $nfiles = 0 ]; then echo "ERROR: No files to test"; exit 1; fi
 
+##--------
+gcc -std=gnu99 test_fastdiv.c; ./a.out
 ##--- Generate documentation
 ofile_prefix=_q_tests
 texfile=${ofile_prefix}.tex
 pdffile=${ofile_prefix}.pdf
 cp preamble.tex $texfile
 echo "\\title{Unit Tests for RhashMap in Q}" >> $texfile
-grep '\/\/C'  test_q*.c | \
+while read line; do
+  echo "Documenting $line to $texfile"
+  cat $line | grep '\/\/C'  | \
   sed s'/\/\/C/ /'g | \
-  sed s'/^test_q_rha*.*:/ /'g >> $texfile
+  sed s'/^test_q_rha*.*:/ /'g | \
+  sed s'/^[ ]*//'g >> _tempf
+  cat _tempf >> $texfile
+  echo "========="
+
+  cat $line | grep '\/\/C'  | \
+  sed s'/\/\/C/ /'g | \
+  sed s'/^test_q_rha*.*:/ /'g | \
+  sed s'/^[ ]*//'g 
+  
+done < _files
+
 echo "\\end{document}" >> $texfile
 latex2pdf $texfile > $pdffile
 cp $pdffile /tmp/
--- src/test_q_rhashmap_3.c
+++ src/test_q_rhashmap_3.c
@@ -19,6 +19,9 @@ test_incr(
     void
     )
 {
+  //C \section{Test increment}
+  //C \label{test_incr}
+  //C \begin{itemize}
   int status = 0;
   int np = 0; // number of probes
   q_rhashmap_t *hmap = NULL;
@@ -30,6 +33,11 @@ test_incr(
   hmap = q_rhashmap_create(0);
   if ( hmap == NULL ) { go_BYE(-1); }
 
+  //C \item Create a hashmap
+  //C \item Set the update type to ADD
+  //C \item For the same key, perform N puts. In the first put,
+  //C set value to 1, in the second to 2
+  //C \item At the end, the value for that key should be \(N \times (N+1)/2\)
   for ( int i = 0; i < 10000; i++ ) { 
     VALTYPE oldval;
     status = q_rhashmap_put(hmap, key, ++val, Q_RHM_ADD, &oldval, &np);
@@ -41,6 +49,7 @@ test_incr(
   q_rhashmap_destroy(hmap);
   t_stop = RDTSC();
   fprintf(stdout, "Passsed  %s in cycles = %" PRIu64 "\n", __func__, (t_stop-t_start));
+  //C \end{itemize}
 BYE:
   return status;
 }
