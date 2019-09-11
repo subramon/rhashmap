b4d5822d7fcf5e388e65f4adb323d66f3cc76f76
Ramesh Subramonian <subramonian@gmail.com>
2019-09-11 20:00:44 +0000

    ok

--- test/test2.c
+++ test/test2.c
@@ -17,42 +17,29 @@ test_basic(
 {
   int status = 0;
   hmap_t *ptr_hmap = NULL;
-  int N = 10000000;
+  uint32_t N = 10000000;
 
   ptr_hmap = hmap_create(0);
   if ( ptr_hmap == NULL) { go_BYE(-1); }
-  keytype key = 1;
   uint64_t num_probes = 0;
-  valtype chk_oldval = 0;
-  for ( int i = 0; i < N; i++ ) { 
+  for ( uint32_t i = 0; i < N; i++ ) { 
+    valtype altval, oldval;
     bool is_updated, is_found;
-    invaltype inval = i + 1;
-    valtype oldval;
+    keytype key = i+1;
+    invaltype inval = i+1;
     status = hmap_put(ptr_hmap, key, &inval, &oldval, &is_updated, &num_probes);
     cBYE(status);
-    if ( num_probes != 0 ) { go_BYE(-1); }
-    if ( ptr_hmap->nitems != 1 ) { go_BYE(-1); }
-    if ( i == 0 ) { 
-      if ( is_updated == true ) { go_BYE(-1); }
-    }
-    else {
-      if ( is_updated == false ) { go_BYE(-1); }
-      if ( oldval != chk_oldval ) { go_BYE(-1); }
-    }
-    cBYE(status);
-    chk_oldval += inval;
-    valtype altval;
+    if ( ptr_hmap->nitems != i+1 ) { go_BYE(-1); }
+    if ( is_updated == true ) { go_BYE(-1); }
     status = hmap_get(ptr_hmap, key, &altval, &is_found, &num_probes);
     cBYE(status);
-    if ( altval != chk_oldval ) { go_BYE(-1); }
+    if ( altval != inval ) { 
+      printf("key = %lu, inval = %f, altval = %lf \n",
+          key, inval, altval);
+      go_BYE(-1); }
     if ( !is_found ) { go_BYE(-1); }
-    status = hmap_get(ptr_hmap, 123, &altval, &is_found, &num_probes);
-    if ( is_found ) { go_BYE(-1); }
-
   }
-
   hmap_destroy(ptr_hmap); 
-
 BYE:
   return status;
 }
